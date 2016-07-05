a9fe31f3722139c24abf91974677a350fc51107d
DSM-Tech-Academy <dmikkelson39@gmail.com>
2016-07-05 10:12:43 -0700

    Adding more python

---
+++ python/PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -0,0 +1,12 @@
+import random
+
+
+s = 10
+
+def func(step):
+	print (step)
+	print('this is a statement')
+
+
+
+func(s)
---
+++ python/PythonApplication2/PythonApplication2/phones1.py
@@ -0,0 +1,10 @@
+phonelist = [
+  ['Barnhart, Ralph' : '683-2341'],
+  ['Jones, Janet' : '483-5432'],
+  ['Meyers, Chris' : '343-4349'],
+  ['Nelson, Eric' : '485-2689'],
+  ['Prefect, Ford' : '987-6543'],
+  ['Smith, Bob' : '689-1234'],
+  ['Smith, Robert' : '689-1234'],
+  ['Zigler, Mary' : '567-8901']
+]
